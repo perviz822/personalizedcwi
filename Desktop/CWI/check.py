61188e80cb868c3daa6c0f6598152f49b29a5cff
Parviz <perviz822@gmail.com>
2025-02-08 12:55:40 +0100

    datasets were combined into one

--- Desktop/CWI/check.py
+++ Desktop/CWI/check.py
@@ -21,8 +21,20 @@ import pandas as pd
 
 '''
 '''
-df =  pd.read_csv('English_News_Dev.csv')
-print(len(df))
+
+
+'''
+
+'''
+df = pd.read_csv("English_Wikipedia_Train.tsv", sep="\t")  # Read TSV
+df.to_csv("English_Wikipedia_Train.csv", index=False)  # Save as CSVxs
+
+'''
+
+
+
+df =  pd.read_csv('English_Wikipedia_Articles/train.csv')
+
 
 
 df.columns = [
@@ -37,13 +49,17 @@ df.columns = [
 ]
 
 
-df.to_csv("English_WikiNews_Dev.csv", index=False)
-
-'''
-
-'''
+df.to_csv("English_Wikipedia_Articles/train.csv", index=False)
 
 
-'''
-df = pd.read_csv("English_Wikipedia_Train.tsv", sep="\t")  # Read TSV
-df.to_csv("English_Wikipedia_Train.csv", index=False)  # Save as CSVxs
+def merge_csv_files(file1,file2,file3,merged_file_name):
+    df1 = pd.read_csv(file1)
+    df2 = pd.read_csv(file2)
+    df3 = pd.read_csv(file3)
+    merged_df = pd.concat([df1, df2, df3], axis=0, ignore_index=True)
+    merged_df.to_csv(merged_file_name, index=False)
+    pass;
+# Save merged data to a new CSV
+
+
+merge_csv_files('English_Wikipedia_Articles/test.csv','English_Professional_News/test.csv','English_Wiki_News/test.csv','test.csv')
